311e4b5cdd9ecf3d3667c96f1e81b1458c32144f
sergioguilherme1 <sergioguilherme.rp@gmail.com>
2026-06-06 01:23:34 -0300

    feat: apply migrations automatically on startup

--- src/Program.cs
+++ src/Program.cs
@@ -4,7 +4,9 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+        options.JsonSerializerOptions.Converters.Add(new System.Text.Json.Serialization.JsonStringEnumConverter()));
 builder.Services.AddOpenApi();
 
 builder.Services.AddDbContext<BolaoDbContext>(options =>
@@ -15,6 +17,12 @@ builder.Services.AddScoped<ApostaService>();
 builder.Services.AddScoped<ApuracaoService>();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<BolaoDbContext>();
+    db.Database.Migrate();
+}
 
 if (app.Environment.IsDevelopment())
 {
